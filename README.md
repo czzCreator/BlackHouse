ea166b9b24c864cddf134910b51656eed9bc29a3
zhizhong_chen <zhizhong_chen@foxitsoftware.com>
2023-09-01 23:16:58 +0800

    add new

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# shanwujinghun
+# blackhouse
 
-A new Flutter application.
+Betrayal at house on the hill
 
 ## Getting Started
 
@@ -14,33 +14,3 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
-
-plans
-1.首页面重构
-2.游戏主页面重构
-3.三个地图块初始化实现重构
-4.三块地图的上下左右箭头重构
-5.抽取新地图的重构（是否合适放置）
-6.新地图的放置方向选择重构
-7.人员显示的重构
-8.人员走动的重构
-9.人员属性的重构显示
-//10.人员任务面板的重构
-//人员物品界面，卡牌界面的编写
-11.骰子的重构
-12.小地图的重构
-13.事件卡的重构
-14.物品卡的重构
-15.征兆卡的重构
-16.抽卡的衔接模块（弹出界面时，遮罩层HasWidgetsOverlay）
-17.事件卡的具体界面细化
-18.物品卡的具体界面细化
-19.征兆卡的具体界面细化
-20.怪物类的实现
-21.征兆卡预测故事线的触发
-22.场景切换实现（框架设计与搭建）
-23.剧本及各角色任务编写
-24.结束任务编写，结束条件判断
-25.当前人员状态汇总展示
-26.具体实现一个脚本
-待续
--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,9 +2,9 @@ package io.flutter.plugins;
 
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
-import io.flutter.Log;
 
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 
 /**
  * Generated file. Do not edit.
@@ -13,7 +13,9 @@ import io.flutter.embedding.engine.FlutterEngine;
  */
 @Keep
 public final class GeneratedPluginRegistrant {
-  private static final String TAG = "GeneratedPluginRegistrant";
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
+      xyz.luan.audioplayers.AudioplayersPlugin.registerWith(shimPluginRegistry.registrarFor("xyz.luan.audioplayers.AudioplayersPlugin"));
+    flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
   }
 }
