670b9eadc8d8ed8be8813ad7816677a42fc6440a
zhizhong_chen <zhizhong_chen@foxitsoftware.com>
2023-09-07 00:05:03 +0800

    WidgetsBinding 和 window——manger 类使用 用于管理配置 主窗口 同时增加 主窗口事件 监听回调入口

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,52 @@
-import 'package:betrayal_at_house/widgets/black_house_game.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+//如何自定义桌面窗口
+//http://bbs.itying.com/topic/643548234aa6b50d24f8542b
+import 'package:window_manager/window_manager.dart';
+
+import './black_house_game.dart';
+import './widgets/widgets.dart';
+
+void main() async {
+  //保证Widgets初始化
+  /*
+  在 Flutter 中,WidgetsFlutterBinding类的主要作用有:
+1. 初始化Flutter框架
+WidgetsFlutterBinding的ensureInitialized()方法会负责初始化整个Flutter框架,确保各个子系统都准备就绪。这一步非常重要,需要在运行runApp()之前完成。
+2. 提供对根Widget的访问
+WidgetsBinding可以通过instance访问当前活跃的根Widget,这为获取context提供了可能。
+3. 管理窗口大小和 Locale
+WidgetsBinding可以获取窗口信息,还可以监听窗口大小改变等事件。可以设置Locale来更改系统语言环境。
+4. 提供 Scheduler 绑定
+WidgetsBinding内部含有一个SchedulerBinding实例,它管理着UI绘制、布局等任务的调度。
+5. 提供平台消息通道
+WidgetsBinding包含一个默认的平台通道,可以用来与原生代码通信。
+6. 提供应用生命周期监听
+可以通过WidgetsBinding监听应用生命周期事件,如暂停、恢复、内存压力等。
+7. pump函数重新绘制UI
+WidgetsBinding的drawFrame()方法可以手动重新绘制UI界面。
+所以在Flutter应用启动前,需要先调用WidgetsFlutterBinding的ensureInitialized()进行初始化,而它正是承担了这些重要工作,是Flutter框架正常运行的基础。
+  */
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 必须加上这一行。windowManager 库的初始化
+  await windowManager.ensureInitialized();
+
+  // 设置主窗口属性
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(600, 800),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.normal,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const GameApp());
 }
 
@@ -65,7 +109,20 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+//增加对 主窗口事件的监听拓展 (WindowListener)
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -83,8 +140,8 @@ class _MyHomePageState extends State<MyHomePage> {
           builder: (context, constraints) {
             return Container(
                 constraints: const BoxConstraints(
-                  maxWidth: 1920,
-                  maxHeight: 1080,
+                  maxWidth: double.infinity,
+                  maxHeight: double.infinity,
                 ),
                 child: GameWidget(
                     game: game,
@@ -102,14 +159,77 @@ class _MyHomePageState extends State<MyHomePage> {
                           (BuildContext context, BlackHouseGame game) {
                         return const Text('mainMenuOverlay');
                       },
-                      'gameOverlay':
+                      'gameOverOverlay':
                           (BuildContext context, BlackHouseGame game) {
-                        return const Text('gameOverlay');
+                        return GameOverOverlay(game);
                       },
                     }));
           },
         ),
       ),
     );
+  }
+
+  /*
+  * 以下是各种 主窗口事件的监听回调执行方法
+  */
+  @override
+  void onWindowEvent(String eventName) {
+    //print('[WindowManager] onWindowEvent: $eventName');
+  }
+
+  @override
+  void onWindowClose() {
+    // do something
+  }
+
+  @override
+  void onWindowFocus() {
+    // do something
+  }
+
+  @override
+  void onWindowBlur() {
+    // do something
+  }
+
+  @override
+  void onWindowMaximize() {
+    // do something
+  }
+
+  @override
+  void onWindowUnmaximize() {
+    // do something
+  }
+
+  @override
+  void onWindowMinimize() {
+    // do something
+  }
+
+  @override
+  void onWindowRestore() {
+    // do something
+  }
+
+  @override
+  void onWindowResize() {
+    // do something
+  }
+
+  @override
+  void onWindowMove() {
+    // do something
+  }
+
+  @override
+  void onWindowEnterFullScreen() {
+    // do something
+  }
+
+  @override
+  void onWindowLeaveFullScreen() {
+    // do something
   }
 }
